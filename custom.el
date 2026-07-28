9bf8ff1a767d8ff4d3f9c7da72f88fe7ef3d2954
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2026-07-28 08:21:47 +0200

    minor fixes

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-fold-catch-invisible-edits 'show-and-error nil nil "Customized with use-package org")
  '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((modus-nordic-night-theme :url
